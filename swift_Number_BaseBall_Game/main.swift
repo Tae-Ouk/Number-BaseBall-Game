bd5c0650742f5758d6eccda5d707bee593aad263
Tae-Ouk <to99820@naver.com>
2024-03-14 14:36:47 +0900

    1st Level

--- swift_Number_BaseBall_Game/main.swift
+++ swift_Number_BaseBall_Game/main.swift
@@ -1,11 +1,51 @@
-//
-//  main.swift
-//  swift_Number_BaseBall_Game
-//
-//  Created by TaeOuk Hwang on 3/14/24.
-//
+let game = BaseballGame()
+game.start()
 
-import Foundation
 
-print("Hello, World!")
+struct BaseballGame {
+    func start() {
+        let answer = makeAnswer()
+        
+        
+        print("Start Number BaseBall Game")
+        print("Input Your Number")
+        
+        print(makeAnswer())
+        
+        let input = readLine()!
+        
+        let IntAnswer = String(input).map{ Int(String($0))! }
+        // 입력받은 input을 String -> [Int]로 변환
+        
+        print(IntAnswer)
+        
+        
+        
+    }
+    
+    
+    func makeAnswer() -> [Int] {
+        var ansMake = Array(1...9)
+        var randomValues: [Int] = []
 
+        while randomValues.count < 3 {
+            let randomIndex = Int.random(in: 0..<ansMake.count)
+            let randomNumber = ansMake[randomIndex]
+            
+            if !randomValues.contains(randomNumber) {
+                randomValues.append(randomNumber)
+            }
+        }
+        
+        /*
+        var ansMake: [Int] = []
+        
+        for i in 0...2 {
+            ansMake.append(Int.random(in: 1...9))
+        }
+         // ⚠️ 랜덤하게 추가된 ansMake의 요소 중 중복이 발생할 수 있음 -> ✅ 해결!
+        */
+        
+        return randomValues
+    }
+}
